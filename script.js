5aedc4352032984ce03d5ffc893fd0abab2d019d
joyetgeorge <joyetgeorge07@gmail.com>
2023-01-27 14:03:57 +0530

    Landing page gsap timeline added

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
-// gsap.from('.header', { duration: 1, y: '-100%', ease: 'bounce' })
-// gsap.from('.link', { duration: 1, opacity: 0, delay: 1, stagger: .5 })
-// gsap.from('.right', { duration: 1, x: '-100vw', delay: 1, ease: 'power2.in' })
-// gsap.from('.left', { duration: 1, delay: 1.5, x: '-100%' })
-// gsap.to('.footer', { duration: 1, y: 0, ease: 'elastic', delay: 2.5 })
-// gsap.fromTo('.button', { opacity: 0, scale: 0, rotation: 720 }, { duration: 1, delay: 3.5, opacity: 1, scale: 1, rotation: 0 })
+var titleWrapper = document.querySelector(".title");
+titleWrapper.innerHTML =  titleWrapper.textContent.replace(
+  /\S/g, "<span class='letter'>$&</span>"
+)
 
+var tl = gsap.timeline({defaults: {duration: 1}});
 
-const timeline = gsap.timeline({ defaults: { duration: 1 }})
-
-timeline
-  .from('.header', { y: '-100%', ease: 'bounce' })
-  .from('.link', { opacity: 0, stagger: .5 })
-  .from('.right', { x: '-100vw', ease: 'power2.in' }, 1)
-  .from('.left', { x: '-100%' }, '<.5')
-  .to('.footer', {  y: 0, ease: 'elastic' })
-  .fromTo('.button', { opacity: 0, scale: 0, rotation: 720 }, { opacity: 1, scale: 1, rotation: 0 })
+tl
+  .from('.letter', { translateY: [-200, 0], stagger: .04 })
+  .from('.subtitle', { duration: 0.4, opacity: 0, translateY: [20, 0]})
+  .from('.nav-item', { opacity: 0, stagger: .04 })
