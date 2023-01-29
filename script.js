f29660eb19178ad24eacf75692ac79f8f9486012
joyetgeorge <joyetgeorge07@gmail.com>
2023-01-29 22:37:55 +0530

    structure changes

--- script.js
+++ script.js
@@ -3,9 +3,25 @@ titleWrapper.innerHTML =  titleWrapper.textContent.replace(
   /\S/g, "<span class='letter'>$&</span>"
 )
 
+gsap.registerPlugin(ScrollTrigger);
+
+
 var tl = gsap.timeline({defaults: {duration: 1}});
 
 tl
   .from('.letter', { translateY: [-200, 0], stagger: .04 })
   .from('.subtitle', { duration: 0.4, opacity: 0, translateY: [20, 0]})
   .from('.nav-item', { opacity: 0, stagger: .04 })
+
+
+gsap.from('.about-title', {
+  scrollTrigger: {
+    trigger: '.about-title',
+    start: '20px 80%',
+    end: 'bottom 100px',
+    markers: true,
+    // scrub: true
+  },
+  opacity: 0,
+  duration: 1
+})
